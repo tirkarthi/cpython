4b99159cabd8767f4bd98429b3cf66bbea456e36
Xtreak <tir.karthi@gmail.com>
2019-08-24 19:32:07 +0530

    Add xml.tool to pretty print XML

---
+++ Lib/xml/tool.py
@@ -0,0 +1,46 @@
+r"""Command-line tool to validate and pretty-print XML
+
+Usage::
+
+    $ echo '<html><body><p>hello</p></body></html>' | python -m xml.tool
+    <html>
+      <body>
+        <p>hello</p>
+      </body>
+    </html>
+    $ echo '<html><body><p>hello</p></body>' | python -m xml.tool
+    no element found: line 2, column 0
+
+"""
+import argparse
+import sys
+from xml.etree import ElementTree as ET
+
+
+def main():
+    prog = 'python -m xml.tool'
+    description = ('A simple command line interface for xml module '
+                   'to validate and pretty-print XML objects.')
+    parser = argparse.ArgumentParser(prog=prog, description=description)
+    parser.add_argument('infile', nargs='?', type=argparse.FileType(),
+                        help='an XML file to be validated or pretty-printed',
+                        default=sys.stdin)
+    parser.add_argument('outfile', nargs='?', type=argparse.FileType('w'),
+                        help='write the output of infile to outfile',
+                        default=sys.stdout)
+    options = parser.parse_args()
+
+    infile = options.infile
+    outfile = options.outfile
+    with infile, outfile:
+        try:
+            elem = ET.XML(infile.read())
+            ET.indent(elem)
+            outfile.write(ET.tostring(elem).decode())
+            outfile.write('\n')
+        except ET.ParseError as e:
+            raise SystemExit(e)
+
+
+if __name__ == '__main__':
+    main()
